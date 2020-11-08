d23f76412af1ad98b17c0d15551f389e9a85c188
Arsh <arshagarwal.18@gmail.com>
2020-11-09 02:09:25 +0530

    adding img_size in run script

--- run_scripts/in_the_wild.sh
+++ run_scripts/in_the_wild.sh
@@ -1,1 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python test.py --name males_model --which_epoch latest --display_id 0 --traverse --interp_step 0.05 --image_path_file males_image_list.txt --in_the_wild --verbose 
+CUDA_VISIBLE_DEVICES=0 python test.py --name males_model --which_epoch latest --display_id 0 --traverse --image_path_file males_image_list.txt --in_the_wild --deploy --interp_step 1 --loadSize 256 --fineSize 256
+
